887be5aadc2cd2ba5945a2bd745bfe8f1ba6b58d
Nikolausinsulin <76692911+Nikolausinsulin@users.noreply.github.com>
2026-07-23 15:22:20 +0200

    a

---
+++ materialModels.py
@@ -0,0 +1,88 @@
+import ufl
+import dolfinx
+
+
+def neoHook(domain, u, youngsModulus, poissonRatio):
+    # Define kinematic quantities used in the problem
+    # Spatial dimension
+    d = len(u)
+
+    # Identity tensor
+    I = ufl.variable(ufl.Identity(d))
+
+    # Deformation gradient
+    F = ufl.variable(I + ufl.grad(u))
+
+    # Right Cauchy-Green tensor
+    C = ufl.variable(F.T * F)
+
+    # Invariants of deformation tensors
+    Ic = ufl.variable(ufl.tr(C))
+    J = ufl.variable(ufl.det(F))
+
+    # Define the elasticity model via a stored strain energy density function W, and create the expression for the first Piola-Kirchhoff stress:
+    # Elasticity parameters
+
+    E = dolfinx.default_scalar_type(youngsModulus)
+    nu = dolfinx.default_scalar_type(poissonRatio)
+    mu = dolfinx.fem.Constant(domain, E / (2 * (1 + nu)))
+    lmbda = dolfinx.fem.Constant(domain, E * nu / ((1 + nu) * (1 - 2 * nu)))
+
+    # Stored strain energy density (compressible neo-Hookean model)
+    W = (mu / 2) * (Ic - 3) - mu * ufl.ln(J) + (lmbda / 2) * (ufl.ln(J)) ** 2
+
+    # Hyper-elasticity
+    P = ufl.diff(W, F)
+
+    return P
+
+
+def neoHookIncompressible(domain, u):
+    c_10 = 10
+
+    print(f"neoHookIncompressible with C_10={c_10}")
+
+    C_10 = dolfinx.fem.Constant(domain, dolfinx.default_scalar_type(c_10))
+
+    d = len(u)
+    I = ufl.variable(ufl.Identity(d))
+    F = ufl.variable(I + ufl.grad(u))
+    C = ufl.variable(F.T * F)
+
+    # see https://en.wikipedia.org/wiki/Invariants_of_tensors
+    I_1 = ufl.tr(C)
+    I_2 = 0.5 * (ufl.tr(C) ** 2 - ufl.tr(C * C))
+
+    W = C_10 * (I_1 - 3)
+
+    P = ufl.diff(W, F)
+    return P
+
+
+def simplifiedMooneyRivlinTP1(domain, u):
+    c_01 = 17.4
+    c_10 = -11.11
+    c_02 = 3.134
+
+    # c_01 = 1
+    # c_10 = -1
+    # c_02 = 1
+    print(f"simplifiedMooneyRivlinTP1 with C_01={c_01}, C_10={c_10}, C_02={c_02}")
+
+    C_01 = dolfinx.fem.Constant(domain, dolfinx.default_scalar_type(c_01))
+    C_10 = dolfinx.fem.Constant(domain, dolfinx.default_scalar_type(c_10))
+    C_02 = dolfinx.fem.Constant(domain, dolfinx.default_scalar_type(c_02))
+
+    d = len(u)
+    I = ufl.variable(ufl.Identity(d))
+    F = ufl.variable(I + ufl.grad(u))
+    C = ufl.variable(F.T * F)
+
+    # see https://en.wikipedia.org/wiki/Invariants_of_tensors
+    I_1 = ufl.tr(C)
+    I_2 = 0.5 * (ufl.tr(C) ** 2 - ufl.tr(C * C))
+
+    W = C_10 * (I_1 - 3) + C_01 * (I_2 - 3) + C_02 * (I_2 - 3) ** 2
+
+    P = ufl.diff(W, F)
+    return P
